02a5505f41561c0d84d6c43e1c68931bfe18a405
Petr Klimenko <petrklim@yandex.ru>
2019-02-25 01:08:07 +0300

    add json decoder

--- src/shared/api/auth.ts
+++ src/shared/api/auth.ts
@@ -1,13 +1,42 @@
 import axios from "axios";
+import * as _ from "jsonous";
 
-function requestCode(phone: string) {
-  console.log(`make real HTTP request ${phone}`);
-  return axios
-    .post("/api/login/request_code", {
-      phone,
-      profile_type: "employer"
-    })
-    .then(() => "success");
-}
+// TODO: write tests for different responses
+// TODO: Handle error case
+const requestCode = async (phone: string) => {
+  const response = await axios.post("/api/login/request_code", {
+    phone,
+    profile_type: "employer"
+  });
+
+  const parsedResult = _.succeed({})
+    .assign("success", _.field("success", _.boolean))
+    .assign(
+      "data",
+      _.field(
+        "data",
+        _.succeed({})
+          .assign("external_id", _.field("external_id", _.string))
+          .assign("expires_in", _.field("expires_in", _.number))
+          .assign(
+            "timeout_expiration_block",
+            _.field("timeout_expiration_block", _.number)
+          )
+      )
+    )
+    .decodeAny(response.data);
+
+  let result: string = "";
+  parsedResult.cata({
+    Ok: ({ data }) => {
+      result = data.external_id;
+    },
+    Err: error => {
+      throw new Error("Parsing error " + error);
+    }
+  });
+
+  return result;
+};
 
 export { requestCode };
